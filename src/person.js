f9ee8bc358a3ae04424d920cfe914a78f972216d
Rachel Selkirk <rachel.selkirk@gmail.com>
2018-07-19 12:49:50 -0700

    added webpack to project

---
+++ src/person.js
@@ -0,0 +1,5 @@
+export const isAdult = (age) => age >=18;
+export const canDrink = (age) => age >=19;
+const isSenior = (age) => age >= 64;
+
+export default isSenior;
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,8 +5,25 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: props.count
+      count: 0
     };
+  }
+  componentDidMount() {
+    try {
+      const stringCount = localStorage.getItem('count');
+      const count = parseInt(stringCount, 10);
+
+      if (!isNaN(count)) {
+        this.setState(() => ({ count }));
+      }
+    } catch (e) {
+      // Do nothing
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
+    }
   }
   handleAddOne() {
     this.setState((prevState) => {
@@ -41,37 +58,4 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-  count: 0
-};
-
 ReactDOM.render(<Counter count={5}/>, document.getElementById('app'));
-
-// let count = 0;
-// const addOne = () => {
-//   count++;
-//   renderCounterApp();
-// };
-// const minusOne = () => {
-//   count--;
-//   renderCounterApp();
-// };
-// const reset = () => {
-//   count = 0;
-//   renderCounterApp();
-// };
-
-// const renderCounterApp = () => {
-//   const templateTwo = (
-//     <div>
-//       <h1>Count: {count}</h1>
-//       <button onClick={addOne}>+1</button>
-//       <button onClick={minusOne}>-1</button>
-//       <button onClick={reset}>Reset</button>
-//     </div>
-//   );
-
-//   ReactDOM.render(templateTwo, appRoot);
-// };
-
-// renderCounterApp();
