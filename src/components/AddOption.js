86150cff4d90a339936b23a11e1266be81edc04a
Rachel Selkirk <rachel.selkirk@gmail.com>
2018-07-21 18:16:17 -0700

    added selected option modal functionality

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -8,7 +8,7 @@ export default class AddOption extends React.Component {
     super(props);
     this.handleAddOption = this.handleAddOption.bind(this);
   }
-  handleAddOption(e) {
+  handleAddOption = (e) => {
     e.preventDefault();
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
@@ -18,7 +18,7 @@ export default class AddOption extends React.Component {
     if (!error) {
       e.target.elements.option.value = '';
     }
-  }
+  };
   render() {
     return (
       <div>
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption.js';
 import Action from './Action.js';
 import Header from './Header.js';
 import Options from './Options.js';
+import OptionModal from './OptionModal.js'
 
 export default class IndecisionApp extends React.Component {
   state = {
-    options: []
+    options: [],
+    selectedOption: undefined
   };
   handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
@@ -19,7 +21,9 @@ export default class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    alert(option);
+    this.setState(() => ({
+      selectedOption: option
+    }));
   };
   handleAddOption = (option) => {
     if (!option) {
@@ -31,6 +35,11 @@ export default class IndecisionApp extends React.Component {
       options: prevState.options.concat(option) 
     }));
   };
+  handleClearSelectedOption = () => {
+    this.setState(() => ({
+      selectedOption: undefined
+    }));
+  }
   componentDidMount() {
     try {
       const json = localStorage.getItem('options');
@@ -67,6 +76,10 @@ export default class IndecisionApp extends React.Component {
         />
         <AddOption
           handleAddOption={this.handleAddOption}
+        />
+        <OptionModal 
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
         />
       </div>
     )
