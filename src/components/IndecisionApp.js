4e24c06215508e04f8e21194d070db5e8b537d0a
Rachel Selkirk <rachel.selkirk@gmail.com>
2019-05-27 12:21:49 -0700

    Revert "add classes"
    This reverts commit 13bddf1a9c1d14ab4174a2ce357d55ecc8dd280d.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -65,22 +65,18 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <div className="container">
-          <Action
-            hasOptions={this.state.options.length > 0}
-            handlePick={this.handlePick}
-          />
-          <div className="widget">
-            <Options
-              options={this.state.options}
-              handleDeleteOptions={this.handleDeleteOptions}
-              handleDeleteOption={this.handleDeleteOption}
-            />
-            <AddOption
-              handleAddOption={this.handleAddOption}
-            />
-          </div>
-        </div>
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
         <OptionModal 
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 const Option = (props) => (
-  <div className="option">
-    <p className="option__text">{props.count}. {props.optionText}</p>
+  <div>
+    {props.optionText}
     <button
-      className="button button--link"
       onClick={(e) => {
         props.handleDeleteOption(props.optionText);
-        }}
+      }}
     >
       Remove
     </button>
